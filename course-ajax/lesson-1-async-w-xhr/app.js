02c19d2f3fe12d20642d905afeb2202a73644f9e
Erin <ecmgit@outlook.com>
2018-09-07 13:23:42 +0800

    ADD: NYTimes api integration

--- course-ajax/lesson-1-async-w-xhr/app.js
+++ course-ajax/lesson-1-async-w-xhr/app.js
@@ -10,6 +10,48 @@
     const nyTimesAPIKey = `66d918fae86549ae83a55edf4785f4c0`;
 
 
+    function addImage(data) {
+        const images = data.results;
+        let htmlContent = `<div class="error-no-articles"><h3>There were no images found</h3></div>`
+
+        if (images[0]) {
+            htmlContent = ``;
+            let num = 0
+            for (const image of images) {
+                const img = `<figure>
+                    <img src="${image.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${image.user.name}</figcaption>
+                </figure>`;
+                htmlContent += img;
+                if (num >= 3)
+                    break;
+                else
+                    num++
+            }
+            // htmlContent = `<figure>
+            //     <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+            //     <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            // </figure>`;
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin',htmlContent);
+    }
+
+    function addArticle(data) {
+        const arts = data.response;
+        let htmlContent = `<div class="error-no-articles"><h3>No articles available</h3></div>`
+
+        if(arts && arts.docs && arts.docs.length > 1) {
+            htmlContent = '<ul class="articles">' + arts.docs.map(article => `<li class="article">
+            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+            </li>`
+            ).join('') + '</ul>'
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
@@ -20,29 +62,9 @@
                 'Authorization':`Client-ID ${unsplashClientID}`
             }
         }).done(addImage);
+        $.ajax({
+            url:`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=66d918fae86549ae83a55edf4785f4c0`
+        }).done(addArticle);
     });
-
-
-    function addImage(data) {
-        const images = data.results;
-        let htmlContent = `<div><h3>There were no images found</h3></div>`
-
-        if (images[0]) {
-            htmlContent = ``;
-            for (const image of images) {
-                const img = `<figure>
-                    <img src="${image.urls.regular}" alt="${searchedForText}">
-                    <figcaption>${searchedForText} by ${image.user.name}</figcaption>
-                </figure>`;
-                htmlContent += img;
-            }
-            // htmlContent = `<figure>
-            //     <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-            //     <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-            // </figure>`;
-        }
-
-        responseContainer.insertAdjacentHTML('afterbegin',htmlContent);
-    }
 
 })();
